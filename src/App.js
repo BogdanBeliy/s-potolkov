0a57abcfb8c877dc2109e5c5ffe31768b6149bd1
BogdanBeliy <uxui.des@gmail.com>
2020-09-01 15:33:57 +0300

    whithout backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, BrowserRouter } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import Odnourovnevie from './Components/ConstructPage/Odnourovnevie/Odnourovnevie';
+import Dvuhurovnevie from './Components/ConstructPage/Dvuhurovnevie/Dvuhurovnevie';
+import DvuhPodsv from './Components/ConstructPage/DvuhPodsv/DvuhPodsv';
+import Par from './Components/ConstructPage/Par/Par';
+import ParLine from './Components/ConstructPage/ParLine/ParLine';
 
-function App() {
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="appContainer">
+      <BrowserRouter>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/odnourovnevie/" component={Odnourovnevie}/>
+      <Route exact path="/dvuhurovnevie/" component={Dvuhurovnevie}/>
+      <Route exact path="/dvuhurovnevie-s-podsvetkoi/" component={DvuhPodsv}/>
+      <Route exact path="/paraschii-potolok/" component={Par}/>
+      <Route exact path="/paraschie-linii/" component={ParLine}/>
+
+
+      </BrowserRouter>
+   
+
+      </div>
+
+   
   );
+}
 }
 
 export default App;
